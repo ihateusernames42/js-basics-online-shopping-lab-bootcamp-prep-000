75c878b761ca791431975c44341d980f25af1302
Dylan L Jones <darkraikiri@gmail.com>
2018-03-02 05:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ if (cardNumber === null){
   console.log("Sorry, we don't have a credit card on file for you.")
 }
 else {
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.)
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
 }
   
 }
