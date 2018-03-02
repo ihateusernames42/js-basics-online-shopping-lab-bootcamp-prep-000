394e44171b8667d1417b8fc77ba4e9d1f8935763
Dylan L Jones <darkraikiri@gmail.com>
2018-03-02 05:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,5 +75,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
 if (cardNumber === null){
-  console.log("")
+  console.log("Sorry, we don't have a credit card on file for you.")
+}
+else {
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.)
+}
+  
 }
